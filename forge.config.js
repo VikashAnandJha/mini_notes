372244f02e7bfd5faa86e643a77d0dd0173f1db8
VikashAnandJha <mailtovikashjha@gmail.com>
2023-02-06 19:16:51 +0530

    yml

--- forge.config.js
+++ forge.config.js
@@ -6,10 +6,22 @@ module.exports = {
       name: '@electron-forge/maker-squirrel',
       config: {
         // An URL to an ICO file to use as the application icon (displayed in Control Panel > Programs and Features).
-        iconUrl: './img/appicon.ico',
+        iconUrl: 'https://raw.githubusercontent.com/VikashAnandJha/mini_notes/main/img/appicon.ico',
         // The ICO file to use as the icon for the generated Setup.exe
         setupIcon: './img/appicon.ico',
       },
     }
+  ],publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'VikashAnandJha',
+          name: 'mini_notes',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
   ],
 };
